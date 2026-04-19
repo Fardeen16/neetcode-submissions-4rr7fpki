d1aa6404c80b198cd62177d41b64c698d30b52c6
Fardeen Khan <81024081+Fardeen16@users.noreply.github.com>
2026-04-19 13:51:31 -0700

    Add: longest-increasing-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-3.py
@@ -0,0 +1,46 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        '''
+        can be doen in O(nlogn) too
+        '''
+
+        # O(n**2) soln:
+        # LIS = [1] * len(nums)
+
+        # for i in range(len(nums)-1, -1, -1):
+        #     for j in range(i, len(nums)):
+        #         if nums[i] < nums[j]:
+        #             LIS[i] = max(LIS[i], 1 + LIS[j])
+        # return max(LIS)
+
+        '''
+        O(nlogn) soln.
+
+        So at each step, we binary search and replace the right most number        
+
+        '''
+        ans = [nums[0]]
+
+        def binary_search(num):
+            l, r = 0, len(ans)-1
+            while l < r:
+                mid = (l+r)//2
+                if ans[mid] < num:
+                    l = mid + 1
+                else:
+                    r = mid
+            return r
+
+        # ietrate for all ns
+        for n in nums[1:]:
+
+        # if less, then bnray search to insert
+            if n <= ans[-1]:
+                index = binary_search(n)
+                ans[index] = n
+            # else just append
+            else:
+                ans.append(n)
+            print(ans)
+        # return max len formed so far 
+        return len(ans)
